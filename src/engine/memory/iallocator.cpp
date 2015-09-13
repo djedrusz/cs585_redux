fc451e264e41661248ed39d4312a55d26c90fc3a
djedrusz <djedrusz@stevens.edu>
2015-09-13 18:40:17 -0400

    Changed "Allocator" to "IAllocator" to show that it specifies an interface. Added explanation for dynamic array growth and shrink procedures.

---
+++ src/engine/memory/iallocator.cpp
@@ -0,0 +1,8 @@
+/*
+	Dominik Jedruszczak
+
+	iallocator.cpp
+	The implementation of the "IAllocator" interface.
+*/
+
+#include "iallocator.hpp"
---
+++ src/engine/memory/iallocator.hpp
@@ -0,0 +1,25 @@
+/*
+	Dominik Jedruszczak
+
+	iallocator.hpp
+	Declaration of the "IAllocator" interface.
+*/
+
+#ifndef INCLUDED_IALLOCATOR
+#define INCLUDED_IALLOCATOR
+
+namespace StevensDev{
+	namespace sgdm { // Stevens Game Development Memory.
+		/* The "IAllocator" interface. */
+		template<typename T>
+		class IAllocator {
+			private:
+			public:
+				/* Function(s). */
+				virtual T* allocate(unsigned int count) = 0; // Allocate the specified amount of memory and return a pointer to the newly created memory block.
+				virtual void deallocate(T* memoryBlock, unsigned int count) = 0; // Deallocate the specified amount of memory from the specified memory block.
+		};
+	}
+}
+
+#endif
